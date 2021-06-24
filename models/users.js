2873c31b5bc9162eca6cdef10604ef6527edb5f5
adigaboy <adiga123456@gmail.com>
2021-06-24 22:41:24 +0300

    hashing password, not returning it at get /users

--- models/users.js
+++ models/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const isEmpty = require('../utils/utils')
+const isEmpty = require('../utils/utils');
 router.use(express.json());
-const db = require('../db/index')
+const db = require('../db/index');
+const crypto = require('crypto');
 
 
 router.get('/', (req, res) => {
-    db('users').select().then(
+    db('users').select('user_id', 'name', 'email').then(
         data => {
             res.status(200).json(data);
         }).catch(
@@ -21,7 +22,7 @@ router.post('/', (req, res) => {
     if (isEmpty(req.body) || !req.body.name || !req.body.email || !req.body.password) {
         return res.status(400).json({"error": "Please send name, email and password in the request body"});
     }
-    db('users').insert({name: req.body.name, email: req.body.email, password: req.body.password}).then(
+    db('users').insert({name: req.body.name, email: req.body.email, password: crypto.createHash('md5').update(req.body.password).digest('hex')}).then(
         data => {
             console.log('added user');
             res.sendStatus(200);
